5bf8e3f365046865c7736037a92fe3c5ebabfd4f
meecan <cannhann42@gmail.com>
2023-06-22 19:47:52 +0300

    Plugin foundation has been created.

--- validation.js
+++ validation.js
@@ -1,1 +1,55 @@
+HTMLElement.prototype.validation = function() {
 
+    // Creating default variables.
+    const form = this
+    const inputs = form.querySelectorAll('input, textarea')
+
+
+
+    // Styling the valid input element.
+    const inputSetValid = (el) => {
+        el.classList.remove('is-invalid')
+        el.classList.add('is-valid')
+    }
+    
+
+
+    // Styling the invalid input element.
+    const inputSetInvalid = (el) => {
+        el.classList.remove('is-valid')
+        el.classList.add('is-invalid')
+    }
+
+
+
+    // Checking whether an input element is valid or not.
+    const checkInput = (el) => {
+        if (el.checkValidity()) {
+            return true
+        }else{
+            return false
+        }
+    }
+
+
+
+    // Filtering process of the input element.
+    const filterInput = (el) => {
+        if (checkInput(el)) {
+            inputSetValid(el)
+        } else {
+            inputSetInvalid(el)
+        }
+    }
+
+
+
+    // Triggering the filtering process for all input items.
+    inputs.forEach(el => {
+        el.classList.add('validate-input')
+        el.parentNode.classList.add('validate-label')
+
+        el.oninput = () => filterInput(el)
+    })
+
+}
